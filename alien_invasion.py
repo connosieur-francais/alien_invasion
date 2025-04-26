335d6fa2fc41313af7fece363fbebe703653f9a8
Monsieur Eason <reasonalworkplace@gmail.com>
2025-04-26 19:33:22 +0800

    Creating an instance of the Alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -47,6 +47,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -63,6 +64,9 @@ class AlienInvasion:
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        
+        self._create_fleet()
         
     def run_game(self):
         """Start the main loop for the game."""
@@ -118,6 +122,13 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+                
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        # Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
+        
     
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen"""
@@ -127,6 +138,7 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
         
         # Make the most recently drawn screen visible
         pygame.display.flip()
