882f4250ea860d182dc12d21051d6f3b54fdc0bf
Monsieur Eason <reasonalworkplace@gmail.com>
2025-04-26 14:20:46 +0800

    Started Project

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+"""
+ALIEN INVASION - USING PYGAME
+Question is gotten from: Python Crash Course EBOOK
+
+In the Alien Invasion project, you'll use the Pygame package to develop a 2D game.
+The goal of the game is to shoot down a fleet of aliens as they drop down the screen,
+in levels that increase in speed and difficulty.
+At the end of the project,
+you'll have learned skills that will enable you to develop your own 2D games in python.
+
+ALIEN INVASION spans a number of different files, so make a new alien_invasion folder on your system.
+Be sure to save all files for the project to this folder, so your import statements will work correctly.
+    Also, if you feel comfortable using version control, you might want to use it for this project.
+    If you haven't used version control before, this is a good time to start.
+    
+----> PLANNING THE PROJECT <----
+
+1. Player controls the ship that appears at the bottom center of the screen.
+
+2. Player can move the ship left right using "A" and "D" keys and shoot bullets using "SPACE" key.
+
+3. When the game starts, a fleet of aliens fill the sky and moves across and down the screen.
+
+4. The player shoots and destryos the aliens.
+
+5. If the player destroys all the aliens, a new fleet appears that moves faster than the previous fleet.
+
+6. If any alien hits the ship or reaches the bottom of the screen, the player loses a ship.
+
+7. If the player loses three ships, the game ends.
+
+"""
