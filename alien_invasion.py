5c7d08adcf7679a25b953381919cfece7039ba4f
Monsieur Eason <reasonalworkplace@gmail.com>
2025-04-26 19:46:59 +0800

    Refactoring _create_fleet()

--- alien_invasion.py
+++ alien_invasion.py
@@ -134,11 +134,14 @@ class AlienInvasion:
         
         current_x = alien_width
         while current_x < (self.settings.screen_width - (2 * alien_width) ):
-            new_alien = Alien(self)
-            new_alien_x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
+            self._create_alien(current_x)
             current_x += 2 * alien_width
+    
+    def _create_alien(self, x_position):
+        new_alien = Alien(self)
+        new_alien_x = x_position
+        new_alien.rect.x = x_position
+        self.aliens.add(new_alien)
     
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen"""
