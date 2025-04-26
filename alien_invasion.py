d9ab7b02a02fc757c2fc15f00e626775334b96de
Monsieur Eason <reasonalworkplace@gmail.com>
2025-04-26 16:58:36 +0800

    Refactoring the check_events() and update_screen() methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,6 +35,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -49,23 +50,33 @@ class AlienInvasion:
         # This is a "Surface" which allows game element to be displayed
         pygame.display.set_caption("Alien Invasion")
         
+        self.ship = Ship(self)
+        
         # Set the Background Color
         self.bg_color = (230, 230, 230)
         
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-                    
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+            self.check_events()
+            self.update_screen()
             self.clock.tick(60) # The number inside the () determines how many frames per second the game should run
+    
+    def check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def update_screen(self):
+        """Updates images on the screen, and flip to the new screen"""
+        
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
     
 if __name__ == "__main__":
     # Make a game isntance, and run the game
