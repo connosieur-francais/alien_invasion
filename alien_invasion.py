9a2fcf6d54bbbdcfe2646b44dcbb8eeb7ab78c45
Monsieur Eason <reasonalworkplace@gmail.com>
2025-04-26 18:38:35 +0800

    Deleting Old Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,6 +62,12 @@ class AlienInvasion:
             self.check_events()
             self.ship.update()
             self.bullets.update()
+            
+            # Get rid of bullets that have dissapeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                    
             self.update_screen()
             self.clock.tick(60) # The number inside the () determines how many frames per second the game should run
     
