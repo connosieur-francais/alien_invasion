04598aa5f438090602c21c73cde2a201266c1f9f
Monsieur Eason <reasonalworkplace@gmail.com>
2025-04-26 18:27:56 +0800

    Storing Bullets in a Group

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,6 +36,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -53,12 +54,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self.check_events()
             self.ship.update()
+            self.bullets.update()
             self.update_screen()
             self.clock.tick(60) # The number inside the () determines how many frames per second the game should run
     
